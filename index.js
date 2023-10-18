a85812415e65a46e2910c8d4139fca6b9f9470cf
Ivan herald <ivanheraldd@gmail.com>
2023-10-19 01:03:03 +0530

    cors update1

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ const app=express();
 app.use(express.json());
 const allowedOrigin = 'https://gifty-frontend.vercel.app';
 const corsOptions = {
-  origin: allowedOrigin,
-};
-app.use("/gift",userRouter);
+    origin: allowedOrigin,
+  };
 app.use(cors(corsOptions));
+
+
+app.use("/gift",userRouter);
+
 mongoose.connect('mongodb+srv://ivanherald:1234567!@gift.wuldils.mongodb.net/?retryWrites=true&w=majority'
 ).then(()=>
-    app.listen(5000,()=>
+    app.listen(3000,()=>
      console.log("Connected to Database And Server is running")
      )
     ).catch(e=>console.log(e));
