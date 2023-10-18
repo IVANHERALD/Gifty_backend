ef7e4518a7eb3f94c31e3daade2ff79dcc5bf7d4
Ivan herald <ivanheraldd@gmail.com>
2023-10-19 00:57:07 +0530

    cors update

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ import cors from 'cors';
 import userRouter from './Routes/routes.js';
 const app=express();
 app.use(express.json());
+const allowedOrigin = 'https://gifty-frontend.vercel.app';
+const corsOptions = {
+  origin: allowedOrigin,
+};
 app.use("/gift",userRouter);
-app.use(cors());
+app.use(cors(corsOptions));
 mongoose.connect('mongodb+srv://ivanherald:1234567!@gift.wuldils.mongodb.net/?retryWrites=true&w=majority'
 ).then(()=>
     app.listen(5000,()=>
